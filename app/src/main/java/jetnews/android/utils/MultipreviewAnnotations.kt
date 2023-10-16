cd984819da900c45767e7419ac88f40e52d2babc
Rishabh Gupta <rishabhgupta2804@gmail.com>
2023-10-16 12:59:46 +0530

    implemented some utils for project

---
+++ app/src/main/java/jetnews/android/utils/MultipreviewAnnotations.kt
@@ -0,0 +1,42 @@
+package jetnews.android.utils
+
+import android.content.res.Configuration.UI_MODE_NIGHT_YES
+import androidx.compose.ui.tooling.preview.Preview
+
+@Preview(
+    name = "small font",
+    group = "font scales",
+    fontScale = 0.5f
+)
+@Preview(
+    name = "large font",
+    group = "font scales",
+    fontScale = 1.5f
+)
+annotation class FontScalePreviews
+
+@Preview(
+    name = "phone",
+    group = "devices",
+    device = "spec:shape=Normal,width=360,height=640,unit=dp,dpi=480"
+)
+@Preview(
+    name = "foldable",
+    group = "devices",
+    device = "spec:shape=Normal,width=673,height=841,unit=dp,dpi=480"
+)
+@Preview(
+    name = "tablet",
+    group = "devices",
+    device = "spec:shape=Normal,width=1200,height=800,unit=dp,dpi=480"
+)
+annotation class DevicePreviews
+
+@Preview(
+    name = "dark thene",
+    group = "themes",
+    uiMode = UI_MODE_NIGHT_YES
+)
+@FontScalePreviews
+@DevicePreviews
+annotation class CompletePreviews
